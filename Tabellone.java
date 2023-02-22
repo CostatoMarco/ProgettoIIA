ada4bc03f4efb90e25bbe709c574fded458a039f
CostatoMarco <m.costato@campus.unimib.it>
2023-02-22 18:14:42 +0100

    Prime classi java

---
+++ Tabellone.java
@@ -0,0 +1,14 @@
+public class Tabellone {
+    private int n;
+    private int M;
+    public Tabellone(int n, int M){
+        this.n = n;
+        this.M = M;
+    }
+    public int getn(){
+        return n;
+    }
+    public int getM(){
+        return M;
+    }
+}
